0b20fe43785057b0109cc1d1ac6fcc98fa8c68e1
Yangha Park <64etuor@gmail.com>
2025-05-21 12:11:21 +0900

    initial commit

---
+++ src/main/java/com/newbit/jenkinspipelinetest/service/CalculatorService.java
@@ -0,0 +1,11 @@
+package com.newbit.jenkinspipelinetest.service;
+
+import com.newbit.jenkinspipelinetest.dto.CalculatorDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public int plusTwoNumbers(CalculatorDTO calculatorDTO) {
+        return calculatorDTO.getNum1() + calculatorDTO.getNum2();
+    }
+}
