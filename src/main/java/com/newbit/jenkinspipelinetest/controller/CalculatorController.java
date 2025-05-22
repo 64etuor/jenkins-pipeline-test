61280a2ad729c6292026709bdaca54d396c630ec
Yangha Park <64etuor@gmail.com>
2025-05-22 09:25:52 +0900

    test

--- src/main/java/com/newbit/jenkinspipelinetest/controller/CalculatorController.java
+++ src/main/java/com/newbit/jenkinspipelinetest/controller/CalculatorController.java
@@ -17,7 +17,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "jenkins pipelintttest tets I'm alive";
+        return "jenkins pipeliest tets I'm alive";
     }
 
     @GetMapping("/plus")
